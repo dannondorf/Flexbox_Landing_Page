b8e24cc23ff7c958fe64bce1ef8bb756584d2d28
Daniel Brett <drummer.man.dan@icloud.com>
2020-10-22 16:19:57 -0500

    Nav Menu Improved
    Added: button hovers, hamburger functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const navMenu = document.querySelector(".main-nav");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function() {
+navMenu.classList.toggle("toggle");
+});
